7daff01171cdafc90f7456df2bfb350dd0209c8b
Mario Villacé <heisenberg.dev@gmail.com>
2015-12-09 20:21:07 +0100

    Adding copyrigth; some improvements on output lines and files.

--- src/es/mvd/TestInetConnectivity.java
+++ src/es/mvd/TestInetConnectivity.java
@@ -1,7 +1,19 @@
+/*******************************************************************************
+ * Copyright (c) 2015 Mario Villacé
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the 
+ * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and limitations under the License.
+ *******************************************************************************/
 package es.mvd;
 
 import java.util.List;
-
 import es.mvd.entity.InetTestResult;
 
 public class TestInetConnectivity {
@@ -13,10 +25,8 @@ public class TestInetConnectivity {
 		}
 		String fileNameIP = args[0];
 		String outFileNameIP = es.mvd.utils.StringUtils.FastDateFormat() + "_out_" + fileNameIP;
-
 		List<String> systemsToTest = es.mvd.utils.FileUtils.leeFicheroIPPuertos(fileNameIP);
 		int numberOfSystemsToTest = (systemsToTest != null)?systemsToTest.size():0;
-
 		if (numberOfSystemsToTest > 0) {
 			String[] outputLines = new String [numberOfSystemsToTest];
 			for(int i = 0; i< numberOfSystemsToTest; i++){
